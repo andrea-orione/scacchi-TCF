fcc01855d57a82c5548240650713fddc4766672d
lmarchetti02 <00lucamarchetti00@gmail.com>
2023-11-26 22:11:12 +0100

    non serve specificare la directory dei file hh

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
-#include "include/Board.hh"
-#include "include/Coordinate.hh"
-#include "include/Movement.hh"
+#include "Board.hh"
+#include "Coordinate.hh"
+#include "Movement.hh"
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // Board provaBoard;
   // provaBoard.printBoardReversed();
 
   // Board provaBoardSemplificata;
   // provaBoardSemplificata.printBoardReversed(true);
 
-  Movement movimentoProva(-1,-1);
+  Movement movimentoProva(-1, -1);
   Coordinate coordinataProva("e5");
 
-  Movement movimentoProva2 = movimentoProva*4;
-  Coordinate coordinataProva2 = coordinataProva+movimentoProva2;
+  Movement movimentoProva2 = movimentoProva * 4;
+  Coordinate coordinataProva2 = coordinataProva + movimentoProva2;
   coordinataProva2.print();
   return 0;
 }
