761e5bbbe9a983cf21ca227439bb707776532008
lmarchetti02 <00lucamarchetti00@gmail.com>
2023-11-29 19:39:39 +0100

    improved factory

--- include/GameManager.hh
+++ include/GameManager.hh
@@ -2,13 +2,19 @@
 
 #include <memory>
 #include <vector>
+#include <array>
+#include <map>
+#include <utility>
 
 #include "Board.hh"
 #include "Piece.hh"
 #include "Bishop.hh"
 
-using std::make_unique;
+using std::array;
+using std::map;
+using std::pair;
 using std::shared_ptr;
+using std::unique_ptr;
 using std::vector;
 
 enum class PieceType
@@ -54,4 +60,39 @@ public:
      * @return The pointer to the piece that has been created created.
      */
     shared_ptr<Piece> makePiece(PieceType pType, PieceColor pColor);
+
+private:
+    /**
+     * Structure containing the positions where to place the white pieces at the beginning of the game.
+     */
+    struct WhiteMap
+    {
+        const array<PieceType, 6> P_TYPE = {PieceType::PAWN, PieceType::ROOK, PieceType::KNIGHT, PieceType::BISHOP, PieceType::QUEEN, PieceType::KING};
+
+        vector<array<int, 2>> ROOK = {{1, 1}, {8, 1}};
+        vector<array<int, 2>> KNIGHT = {{2, 1}, {7, 1}};
+        vector<array<int, 2>> BISHOP = {{3, 1}, {6, 1}};
+        vector<array<int, 2>> KING = {{4, 1}};
+        vector<array<int, 2>> QUEEN = {{5, 1}};
+        vector<array<int, 2>> PAWN = {{1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2}, {6, 2}, {7, 2}, {8, 2}};
+
+        array<vector<array<int, 2>>, 6> COORD = {ROOK, KNIGHT, BISHOP, KING, QUEEN, PAWN};
+    };
+
+    /**
+     * Structure containing the positions where to place the black pieces at the beginning of the game.
+     */
+    struct BlackMap
+    {
+        const array<PieceType, 6> P_TYPE = {PieceType::PAWN, PieceType::ROOK, PieceType::KNIGHT, PieceType::BISHOP, PieceType::QUEEN, PieceType::KING};
+
+        vector<array<int, 2>> ROOK = {{1, 8}, {8, 8}};
+        vector<array<int, 2>> KNIGHT = {{2, 8}, {7, 8}};
+        vector<array<int, 2>> BISHOP = {{3, 8}, {6, 8}};
+        vector<array<int, 2>> KING = {{4, 8}};
+        vector<array<int, 2>> QUEEN = {{5, 8}};
+        vector<array<int, 2>> PAWN = {{1, 7}, {2, 7}, {3, 7}, {4, 7}, {5, 7}, {6, 7}, {7, 7}, {8, 7}};
+
+        array<vector<array<int, 2>>, 6> COORD = {ROOK, KNIGHT, BISHOP, KING, QUEEN, PAWN};
+    };
 };
--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -5,9 +5,19 @@ void GameManager::createWhitePieces()
     PieceColor pColor = PieceColor::WHITE;
     Board &boardInstance = Board::Instance();
 
-    // bishops
-    boardInstance.AddPiece(std::make_pair(Coordinate(3, 1), makePiece(PieceType::BISHOP, pColor)));
-    boardInstance.AddPiece(std::make_pair(Coordinate(6, 1), makePiece(PieceType::BISHOP, pColor)));
+    // get map of white pieces coordinates
+    unique_ptr<WhiteMap> wm = std::make_unique<WhiteMap>();
+
+    for (PieceType pt : wm->P_TYPE)
+    {
+        // explicitly convert PieceType to int
+        int i = static_cast<std::underlying_type_t<PieceType>>(pt);
+
+        for (array<int, 2> xy : wm->COORD[i])
+        {
+            boardInstance.AddPiece(std::make_pair(Coordinate(xy[0], xy[1]), makePiece(pt, pColor)));
+        }
+    }
 }
 
 void GameManager::createBlackPieces()
@@ -15,9 +25,19 @@ void GameManager::createBlackPieces()
     PieceColor pColor = PieceColor::BLACK;
     Board &boardInstance = Board::Instance();
 
-    // bishops
-    boardInstance.AddPiece(std::make_pair(Coordinate(3, 8), makePiece(PieceType::BISHOP, pColor)));
-    boardInstance.AddPiece(std::make_pair(Coordinate(6, 8), makePiece(PieceType::BISHOP, pColor)));
+    // get map of black pieces coordinates
+    unique_ptr<BlackMap> wm = std::make_unique<BlackMap>();
+
+    for (PieceType pt : wm->P_TYPE)
+    {
+        // explicitly convert PieceType to int
+        int i = static_cast<std::underlying_type_t<PieceType>>(pt);
+
+        for (array<int, 2> xy : wm->COORD[i])
+        {
+            boardInstance.AddPiece(std::make_pair(Coordinate(xy[0], xy[1]), makePiece(pt, pColor)));
+        }
+    }
 }
 
 shared_ptr<Piece> GameManager::makePiece(PieceType pType, PieceColor pColor)
