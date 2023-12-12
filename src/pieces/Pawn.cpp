0009074f4c8c4b5ce2c4364b6dbcc65c26ff7c55
lmarchetti02 <00lucamarchetti00@gmail.com>
2023-12-12 14:45:02 +0100

    modified pawn

--- src/pieces/Pawn.cpp
+++ src/pieces/Pawn.cpp
@@ -19,7 +19,7 @@ Pawn::Pawn(PieceColor pColor, Coordinate pPosition)
   }
 }
 
-bool Pawn::isMoveValid(const Coordinate &startingPosition, const Coordinate &endingPosition) const
+bool Pawn::isMoveValid(const Coordinate &endingPosition) const
 {
   return true;
 }
