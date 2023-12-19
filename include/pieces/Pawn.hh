b50931d37312f6283a31c2106f21de33f077bab0
andrea-orione <andrea.orione02@gmail.com>
2023-12-19 13:37:10 +0100

    Prepared merge

--- include/pieces/Pawn.hh
+++ include/pieces/Pawn.hh
@@ -23,4 +23,5 @@ public:
 
 private:
     bool hasMoved;
+    int doubleAdvancementMoveNumber;
 };
--- src/pieces/Pawn.cpp
+++ src/pieces/Pawn.cpp
@@ -5,62 +5,65 @@
 
 Pawn::Pawn(PieceColor pColor, Coordinate pPosition, bool pHasMoved)
 {
-    pieceType = PieceType::PAWN;
-    color = pColor;
-    position = pPosition;
-    hasMoved = pHasMoved;
-    switch (color)
-    {
-    case PieceColor::WHITE:
-        character = "♟︎";
-        literalCharacter = 'P';
-        break;
-    case PieceColor::BLACK:
-        character = "♙";
-        literalCharacter = 'p';
-        break;
-    default:
-        break;
-    }
+  pieceType = PieceType::PAWN;
+  color = pColor;
+  position = pPosition;
+  hasMoved = pHasMoved;
+  doubleAdvancementMoveNumber = -2;
+  switch (color)
+  {
+  case PieceColor::WHITE:
+    character = "♟︎";
+    literalCharacter = 'P';
+    break;
+  case PieceColor::BLACK:
+    character = "♙";
+    literalCharacter = 'p';
+    break;
+  default:
+    break;
+  }
 }
 
 bool Pawn::isMoveValid(const Coordinate endingPosition) const
 {
-    const int xDistance = endingPosition.getX() - this->position.getX();
-    const int yDistance = endingPosition.getY() - this->position.getY();
+  const int xDistance = endingPosition.getX() - this->position.getX();
+  const int yDistance = endingPosition.getY() - this->position.getY();
 
-    const int yDirection = (this->getColor() == PieceColor::WHITE) ? 1 : -1;
+  const int yDirection = (this->getColor() == PieceColor::WHITE) ? 1 : -1;
 
-    if (abs(xDistance) > 1)
-        return false;
-    if (yDistance * yDirection > 2 || yDistance * yDirection < 1)
-        return false;
-    if (this->hasMoved && yDistance * yDirection != 1)
-        return false;
-    if (yDistance * yDirection == 2 && xDistance != 0)
-        return false;
+  if (abs(xDistance) > 1)
+    return false;
+  if (yDistance * yDirection > 2 || yDistance * yDirection < 1)
+    return false;
+  if (this->hasMoved && yDistance * yDirection != 1)
+    return false;
+  if (yDistance * yDirection == 2 && xDistance != 0)
+    return false;
 
-    // mossa
+  // mossa
 
-    Board &board = Board::Instance();
+  Board &board = Board::Instance();
 
-    // Check if landing square is free
+  // Check if landing square is free
 
-    std::shared_ptr<Piece> endingPositionPiece = board.getPiece(endingPosition);
-    if (xDistance == 0 && endingPositionPiece->getColor() != PieceColor::VOID)
-        return false;
-    return !(abs(xDistance) == 1 && endingPositionPiece->getColor() != !this->getColor());
+  std::shared_ptr<Piece> endingPositionPiece = board.getPiece(endingPosition);
+  if (xDistance == 0 && endingPositionPiece->getColor() != PieceColor::VOID)
+    return false;
+  return !(abs(xDistance) == 1 && endingPositionPiece->getColor() != !this->getColor());
 }
 
 std::string Pawn::toString(bool literal) const
 {
-    if (literal)
-        return std::string(1, literalCharacter);
-    return character;
+  if (literal)
+    return std::string(1, literalCharacter);
+  return character;
 }
 
 void Pawn::move(const Coordinate newPosition)
 {
-    position = newPosition;
-    hasMoved = true;
+  Board &boardInstance = Board::Instance();
+  doubleAdvancementMoveNumber = boardInstance.getMoveNumber();
+  position = newPosition;
+  hasMoved = true;
 }
