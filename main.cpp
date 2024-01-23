96c8d386a4cd608358916aa15ea1129ec06e3c90
andrea-orione <andrea.orione02@gmail.com>
2024-01-23 10:08:22 +0100

    Completed remaining todos

--- main.cpp
+++ main.cpp
@@ -3,41 +3,8 @@
 #include <iostream>
 #include <memory>
 
-// TRACK MEMORY ALLOCATIONS (for testing)
-// ---------------------------------------------------------------------------------------
-struct AllocationMetrics
-{
-  uint32_t totalMemoryAllocated = 0;
-  uint32_t totalMemoryFreed = 0;
-
-  uint32_t CurrentMemoryUsage() { return totalMemoryAllocated - totalMemoryFreed; }
-};
-
-static AllocationMetrics allocationMetrics;
-
-void *operator new(size_t size)
-{
-  allocationMetrics.totalMemoryAllocated += size;
-
-  return malloc(size);
-}
-
-void operator delete(void *memory, size_t size) noexcept
-{
-  allocationMetrics.totalMemoryFreed += size;
-
-  free(memory);
-}
-
-static void PrintMemoryUsage()
-{
-  printf("Memory usage: %u bytes\n", allocationMetrics.CurrentMemoryUsage());
-}
-// ---------------------------------------------------------------------------------------
-
 int main(int argc, char *argv[])
 {
-  PrintMemoryUsage();
   try
   {
     std::unique_ptr<GameManager> gm = std::make_unique<GameManager>();
